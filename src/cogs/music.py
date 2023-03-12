818108391e50243a6b6a44f666d545f73a9bdba1
PiotrWieczorek98 <piotr.wieczorek98@gmail.com>
2023-03-12 18:05:05 +0100

    4 spaces indentation to 2 spaces

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -6,53 +6,53 @@ import datetime
 import asyncio
 
 class Music(commands.Cog):
-    """
-    Class for music commands.
-    """
-    def __init__(self, bot: commands.Bot) -> None:
-        self.bot = bot
-        self.queue = []
+  """
+  Class for music commands.
+  """
+  def __init__(self, bot: commands.Bot) -> None:
+    self.bot = bot
+    self.queue = []
 
 
-    def _play(self, interaction):
-        if len(self.queue) > 0:
-            # Play next in queue
-            embed = self._prepare_embed()
-            coro = interaction.channel.send(content=None, embed=embed)
-            asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
-            play_next = self.queue.pop(0)
-            interaction.guild.voice_client.play(play_next, after=lambda e: self._play(interaction))
+  def _play(self, interaction):
+    if len(self.queue) > 0:
+      # Play next in queue
+      embed = self._prepare_embed()
+      coro = interaction.channel.send(content=None, embed=embed)
+      asyncio.run_coroutine_threadsafe(coro, self.bot.loop)
+      play_next = self.queue.pop(0)
+      interaction.guild.voice_client.play(play_next, after=lambda e: self._play(interaction))
 
-    def _prepare_embed(self):
-        current_queue = '\n-'.join(str(element.title) for element in self.queue)
-        embed = discord.Embed(title='The Boi',
-                              color=0x00ff00,
-                              timestamp=datetime.datetime.now(datetime.timezone.utc))
-        embed.add_field(name='queue', value=current_queue)
-        embed.add_field(name='Now Playing', value=f'{self.queue[0].title}')
-        embed.set_footer(text='2137',
-                        icon_url='https://media.tenor.com/mc3OyxhLazUAAAAM/doggo-doge.gif')
-        return embed
+  def _prepare_embed(self):
+    current_queue = '\n-'.join(str(element.title) for element in self.queue)
+    embed = discord.Embed(title='The Boi',
+                          color=0x00ff00,
+                          timestamp=datetime.datetime.now(datetime.timezone.utc))
+    embed.add_field(name='queue', value=current_queue)
+    embed.add_field(name='Now Playing', value=f'{self.queue[0].title}')
+    embed.set_footer(text='2137',
+                    icon_url='https://media.tenor.com/mc3OyxhLazUAAAAM/doggo-doge.gif')
+    return embed
 
-    @app_commands.command(name="yt")
-    async def yt_play(self, interaction: discord.Interaction, url: str):
-        """Plays from a url (almost anything youtube_dl supports)"""
-        await interaction.response.send_message(f"Looking for {url}...")
-        guild = interaction.guild
-        channel = interaction.user.voice.channel
+  @app_commands.command(name="yt")
+  async def yt_play(self, interaction: discord.Interaction, url: str):
+    """Plays from a url (almost anything youtube_dl supports)"""
+    await interaction.response.send_message(f"Looking for {url}...")
+    guild = interaction.guild
+    channel = interaction.user.voice.channel
 
-        if not discord.utils.get(self.bot.voice_clients, guild=guild):
-            await channel.connect()
+    if not discord.utils.get(self.bot.voice_clients, guild=guild):
+      await channel.connect()
 
-        try:
-            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-            self.queue.append(player)
-        except:
-            await interaction.edit_original_response(content=f"Video not available.")
-            return
+    try:
+      player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+      self.queue.append(player)
+    except:
+      await interaction.edit_original_response(content=f"Video not available.")
+      return
 
-        if not guild.voice_client.is_playing():
-            await interaction.edit_original_response(content=f"Now playing {url}.")
-            self._play(interaction)
-        else:
-            await interaction.edit_original_response(content=f"Added {url} to queue.")
+    if not guild.voice_client.is_playing():
+      await interaction.edit_original_response(content=f"Now playing {url}.")
+      self._play(interaction)
+    else:
+      await interaction.edit_original_response(content=f"Added {url} to queue.")
--- src/main.py
+++ src/main.py
@@ -16,71 +16,71 @@ load_dotenv()
 static_ffmpeg.add_paths()
 # Load opus library - depends on OS
 if os.name == 'nt':
-    discord.opus._load_default()
+  discord.opus._load_default()
 elif os.name == 'posix':
-    discord.opus.load_opus('libopus.so.0')
+  discord.opus.load_opus('libopus.so.0')
 if not discord.opus.is_loaded():
-    raise RuntimeError('Opus failed to load!')
+  raise RuntimeError('Opus failed to load!')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("/"),
-    description='The Boi is back',
-    intents=intents,
+  command_prefix=commands.when_mentioned_or("/"),
+  description='The Boi is back',
+  intents=intents,
 )
 
 @bot.event
 async def on_ready():
-    """Event that ocuurence one time when bot is ready to work"""
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+  """Event that ocuurence one time when bot is ready to work"""
+  print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+  print('------')
 
 
 @bot.command()
 @commands.guild_only()
 async def sync(ctx: Context,
-            guilds: Greedy[discord.Object],
-            spec: Optional[Literal["~", "*", "^"]] = None) -> None:
-    """This command sync slash commands with discord"""
-    if not guilds:
-        if spec == "~":
-            synced = await ctx.bot.tree.sync(guild=ctx.guild)
-        elif spec == "*":
-            ctx.bot.tree.copy_global_to(guild=ctx.guild)
-            synced = await ctx.bot.tree.sync(guild=ctx.guild)
-        elif spec == "^":
-            ctx.bot.tree.clear_commands(guild=ctx.guild)
-            await ctx.bot.tree.sync(guild=ctx.guild)
-            synced = []
-        else:
-            synced = await ctx.bot.tree.sync()
+      guilds: Greedy[discord.Object],
+      spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+  """This command sync slash commands with discord"""
+  if not guilds:
+    if spec == "~":
+      synced = await ctx.bot.tree.sync(guild=ctx.guild)
+    elif spec == "*":
+      ctx.bot.tree.copy_global_to(guild=ctx.guild)
+      synced = await ctx.bot.tree.sync(guild=ctx.guild)
+    elif spec == "^":
+      ctx.bot.tree.clear_commands(guild=ctx.guild)
+      await ctx.bot.tree.sync(guild=ctx.guild)
+      synced = []
+    else:
+      synced = await ctx.bot.tree.sync()
 
-        is_spec = 'globally' if spec is None else 'to the current guild.'
-        await ctx.send(
-            f"Synced {len(synced)} commands {is_spec}"
-        )
-        return
+    is_spec = 'globally' if spec is None else 'to the current guild.'
+    await ctx.send(
+      f"Synced {len(synced)} commands {is_spec}"
+    )
+    return
 
-    ret = 0
-    for guild in guilds:
-        try:
-            await ctx.bot.tree.sync(guild=guild)
-        except discord.HTTPException:
-            pass
-        else:
-            ret += 1
+  ret = 0
+  for guild in guilds:
+    try:
+      await ctx.bot.tree.sync(guild=guild)
+    except discord.HTTPException:
+      pass
+    else:
+      ret += 1
 
-    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+  await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
 
 
 async def main():
-    """Main boot start function"""
-    discord.utils.setup_logging(level=logging.DEBUG, root=False)
-    async with bot:
-        await bot.add_cog(Music(bot))
-        await bot.start(os.getenv('BOT_TOKEN'))
+  """Main boot start function"""
+  discord.utils.setup_logging(level=logging.DEBUG, root=False)
+  async with bot:
+    await bot.add_cog(Music(bot))
+    await bot.start(os.getenv('BOT_TOKEN'))
 
 
 asyncio.run(main())

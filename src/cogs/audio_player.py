5058af818b14aa7c42946e7828a66361c322790c
PiotrWieczorek98 <piotr.wieczorek98@gmail.com>
2023-03-14 21:22:01 +0100

    docstrings, namechanges

--- src/cogs/audio_player.py
+++ src/cogs/audio_player.py
@@ -4,7 +4,7 @@ import discord
 import yt_dlp
 from discord.ext import commands
 from discord import app_commands
-from utils.YT_source import YTDLSource
+from utils.youtube_audio import YoutubeSource
 
 
 
@@ -19,7 +19,10 @@ class AudioPlayer(commands.Cog):
     self.queue = {}
     self.embed_handles = {}
 
-  def _add_to_queue(self, guild_id:int, audio_source:YTDLSource):
+  def _add_to_queue(self, guild_id:int, audio_source:YoutubeSource):
+    """
+    Adds audio source to guild's queue
+    """
     guild_queue = self.queue.get(guild_id, [])
     guild_queue.append(audio_source)
     self.queue[guild_id] = guild_queue
@@ -27,7 +30,9 @@ class AudioPlayer(commands.Cog):
 
 
   def _create_embed(self, guild_queue:list):
-    """Creates and returns embed based on guild's queue content"""
+    """
+    Creates and returns embed based on guild's queue content
+    """
     now_playing = guild_queue[0]
     current_queue = ''.join(f"{str(element.title)}\n" for element in guild_queue[1:])
     embed = discord.Embed(title='The Boi',
@@ -47,7 +52,9 @@ class AudioPlayer(commands.Cog):
 
 
   async def _pull_down_embed(self, guild_id:int, text_channel:discord.TextChannel, guild_queue:list):
-    """Removes last message and sends new one to keep it on the bottom of the chat"""
+    """
+    Removes last message and sends new one to keep it on the bottom of the chat
+    """
     current_embed = self.embed_handles.get(guild_id)
     if current_embed:
       await current_embed.delete()
@@ -56,7 +63,9 @@ class AudioPlayer(commands.Cog):
 
 
   def _play_next(self, interaction:discord.Interaction):
-    """Callback function used for players to play next audio source in queue"""
+    """
+    Callback function used for players to play next audio source in queue
+    """
     guild_queue = self.queue.get(interaction.guild_id)
     if guild_queue:
       guild_queue.pop(0)
@@ -75,7 +84,9 @@ class AudioPlayer(commands.Cog):
 
   @app_commands.command(name="yt")
   async def play_yt(self, interaction: discord.Interaction, search_phrase: str):
-    """Plays from a url (almost anything youtube_dl supports)"""
+    """
+    Plays from a url (almost anything youtube_dl supports)
+    """
     await interaction.response.send_message(f"Looking for {search_phrase}...")
     guild = interaction.guild
     voice_channel = interaction.user.voice.channel
@@ -85,7 +96,7 @@ class AudioPlayer(commands.Cog):
       await voice_channel.connect()
 
     try:
-      audio_source = await YTDLSource.from_url(search_phrase, loop=self.bot.loop, stream=True)
+      audio_source = await YoutubeSource.from_url(search_phrase, loop=self.bot.loop, stream=True)
       guild_queue = self._add_to_queue(guild.id, audio_source)
 
       if not guild.voice_client.is_playing():
@@ -102,7 +113,7 @@ class AudioPlayer(commands.Cog):
   @app_commands.command(name="skip")
   async def skip_audio(self, interaction: discord.Interaction):
     """
-    Skip currently played audio
+    Skips currently played audio
     """
     voice_client = interaction.guild.voice_client
     if voice_client and voice_client.is_playing():
@@ -115,7 +126,7 @@ class AudioPlayer(commands.Cog):
   @app_commands.command(name="stop")
   async def stop_player(self, interaction: discord.Interaction):
     """
-    Skip currently played audio
+    Stops currently played audio
     """
     guild = interaction.guild
 
---
+++ src/utils/youtube_audio.py
@@ -0,0 +1,52 @@
+import asyncio
+import yt_dlp
+import discord
+
+ytdl_format_options = {
+  'format': 'bestaudio/best',
+  'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+  'restrictfilenames': True,
+  'noplaylist': True,
+  'nocheckcertificate': True,
+  'ignoreerrors': False,
+  'logtostderr': False,
+  'quiet': True,
+  'no_warnings': True,
+  'default_search': 'auto',
+  'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+  'options': '-vn',
+  'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
+}
+
+ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
+
+
+class YoutubeSource(discord.PCMVolumeTransformer):
+  """
+  Class responsible for obtaining stream source from youtube
+  """
+  def __init__(self, source, *, data, volume=0.5):
+    super().__init__(source, volume)
+
+    self.data = data
+
+    self.title = data.get('title')
+    self.url = data.get('url')
+
+  @classmethod
+  async def from_url(cls, url, *, loop=None, stream=False):
+    """
+    Get audio source from search phrase or direct video url
+    """
+    loop = loop or asyncio.get_event_loop()
+    data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+    if 'entries' in data:
+      # take first item from a playlist
+      data = data['entries'][0]
+
+    filename = data['url'] if stream else ytdl.prepare_filename(data)
+    return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)

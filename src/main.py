50130c021d69d6b54341e1574dd4bbe74308ae18
PiotrWieczorek98 <piotr.wieczorek98@gmail.com>
2023-03-17 15:05:47 +0100

    added restart command, moved commands from main to bot_admin

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 import asyncio
-from typing import Literal, Optional
 import os
 import logging
 from dotenv import load_dotenv
 import discord
-from discord.ext import commands
-from discord.ext.commands import Greedy, Context
 import static_ffmpeg
 from cogs.audio_player import AudioPlayer
+from cogs.bot_admin import BotAdmin
 from utils.boi_bot import BoiBot
 
 # Set up logger
@@ -25,54 +23,11 @@ if not discord.opus.is_loaded():
   raise RuntimeError('Opus failed to load!')
 
 bot = BoiBot()
-
-@bot.event
-async def on_ready():
-  """Event that ocuurence one time when bot is ready to work"""
-  logging.info('Logged in as %s (ID: %d)\n-----------\n', bot.user, bot.user.id)
-
-
-@bot.command()
-@commands.guild_only()
-async def sync(ctx: Context,
-      guilds: Greedy[discord.Object],
-      spec: Optional[Literal["~", "*", "^"]] = None) -> None:
-  """This command sync slash commands with discord"""
-  if not guilds:
-    if spec == "~":
-      synced = await ctx.bot.tree.sync(guild=ctx.guild)
-    elif spec == "*":
-      ctx.bot.tree.copy_global_to(guild=ctx.guild)
-      synced = await ctx.bot.tree.sync(guild=ctx.guild)
-    elif spec == "^":
-      ctx.bot.tree.clear_commands(guild=ctx.guild)
-      await ctx.bot.tree.sync(guild=ctx.guild)
-      synced = []
-    else:
-      synced = await ctx.bot.tree.sync()
-
-    is_spec = 'globally' if spec is None else 'to the current guild.'
-    await ctx.send(
-      f"Synced {len(synced)} commands {is_spec}"
-    )
-    return
-
-  ret = 0
-  for guild in guilds:
-    try:
-      await ctx.bot.tree.sync(guild=guild)
-    except discord.HTTPException:
-      pass
-    else:
-      ret += 1
-
-  await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
-
-
 async def main():
   """Main boot start function"""
   discord.utils.setup_logging(level=logging.WARNING, root=False)
   async with bot:
+    await bot.add_cog(BotAdmin(bot))
     await bot.add_cog(AudioPlayer(bot))
     await bot.start(os.getenv('BOT_TOKEN'))
 
